360cf5a4a51e4131aadf6cf4d597b5eb8b8bbca4
nlfosdic <nlfosdic@mtu.edu>
2017-04-21 13:41:54 -0400

    comments

--- app/src/main/java/edu/mtu/cs3421/voto/Activities/AppCompatPreferenceActivity.java
+++ app/src/main/java/edu/mtu/cs3421/voto/Activities/AppCompatPreferenceActivity.java
@@ -20,6 +20,9 @@ public abstract class AppCompatPreferenceActivity extends PreferenceActivity {
 
     private AppCompatDelegate mDelegate;
 
+    /**
+     * Start activity with saved state to be recreated
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         getDelegate().installViewFactory();
@@ -27,51 +30,81 @@ public abstract class AppCompatPreferenceActivity extends PreferenceActivity {
         super.onCreate(savedInstanceState);
     }
 
+    /**
+     * Used in starting an activity from a saved state
+     */
     @Override
     protected void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
         getDelegate().onPostCreate(savedInstanceState);
     }
 
+    /**
+     * Returns support action bar
+     */
     public ActionBar getSupportActionBar() {
         return getDelegate().getSupportActionBar();
     }
-
+    
+    /**
+     * Sets support action bar
+     */
     public void setSupportActionBar(@Nullable Toolbar toolbar) {
         getDelegate().setSupportActionBar(toolbar);
     }
-
+    
+    /**
+     * Returns menu inflator
+     */
     @Override
     public MenuInflater getMenuInflater() {
         return getDelegate().getMenuInflater();
     }
 
+    /**
+     * Set design of the activity
+     */
     @Override
     public void setContentView(@LayoutRes int layoutResID) {
         getDelegate().setContentView(layoutResID);
     }
 
+    /**
+     * Set design of the activity
+     */
     @Override
     public void setContentView(View view) {
         getDelegate().setContentView(view);
     }
 
+    /**
+     * Set design of the activity
+     */
     @Override
     public void setContentView(View view, ViewGroup.LayoutParams params) {
         getDelegate().setContentView(view, params);
     }
 
+    /**
+     * Set design of the activity
+     */
     @Override
     public void addContentView(View view, ViewGroup.LayoutParams params) {
         getDelegate().addContentView(view, params);
     }
 
+    /**
+     * Called when app is resumed
+     */
     @Override
     protected void onPostResume() {
         super.onPostResume();
         getDelegate().onPostResume();
     }
 
+    /**
+     * Set title for activity
+     */
     @Override
     protected void onTitleChanged(CharSequence title, int color) {
         super.onTitleChanged(title, color);
@@ -84,22 +117,34 @@ public abstract class AppCompatPreferenceActivity extends PreferenceActivity {
         getDelegate().onConfigurationChanged(newConfig);
     }
 
+    /**
+     * Necessary AppCompat method, stop fragments
+     */
     @Override
     protected void onStop() {
         super.onStop();
         getDelegate().onStop();
     }
 
+    /**
+     * Necessary AppCompat method, destroy fragments
+     */
     @Override
     protected void onDestroy() {
         super.onDestroy();
         getDelegate().onDestroy();
     }
 
+    /**
+     * Declare option menu has been changed, and should be recreated
+     */
     public void invalidateOptionsMenu() {
         getDelegate().invalidateOptionsMenu();
     }
 
+    /**
+     * Returns delegate used by activity
+     */
     private AppCompatDelegate getDelegate() {
         if (mDelegate == null) {
             mDelegate = AppCompatDelegate.create(this, null);
